1aa0974e96a637e0ebee96f33f4a7fffe5a7d196
Kris-Mikael Krister <krismikael@gmail.com>
2011-01-05 01:09:12 +0100

    can now edit countries

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,4 +1,5 @@
 class CountriesController < ApplicationController
+  in_place_edit_for :country, :title
   before_filter :check_authorization, :except => [:show, :info]
 
   def show
@@ -18,7 +19,39 @@ class CountriesController < ApplicationController
     end
   end
 
-  def edit 
+  def index
     @countries = Country.all
+    @country = Country.new
+    @continents = Continent.all
+  end
+
+  def create
+    @country = Country.create(params[:country])
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def destroy
+    @country = Country.find(params[:id])
+    @id = @country.id
+    @country.destroy
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def set_country_title
+    country = Country.find(params[:id])
+    old_title = country.title
+    country.title = params[:value]
+
+    if country.save
+      render :text => country.title
+    else
+      render :text => old_title
+    end
   end
 end
