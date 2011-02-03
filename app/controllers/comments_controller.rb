dc45e48dcf0aca3fe13c6a7029f8d53b4bb3d4a0
Kris-Mikael Krister <krismikael@gmail.com>
2011-02-03 21:11:42 +0100

    now possible to remove comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_filter :check_authorization, :except => [:create]
   def create
     @article = Article.find(params[:article_id])
     @comment = Comment.new(params[:comment])
@@ -7,5 +8,12 @@ class CommentsController < ApplicationController
     unless @comment.save
       render :template => 'comments/clear_comment.rjs'
     end
+  end
+
+  def destroy
+    params.each_pair { |key,value| puts "#{key}: #{value}"}
+    comment = Comment.find(params[:id])
+    @id = comment.id
+    comment.destroy
   end
 end
