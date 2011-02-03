1eafff6983123256d7b1b1729f8d87eb13b5994d
Kris-Mikael Krister <krismikael@gmail.com>
2011-02-03 21:17:32 +0100

    more allowed chars

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
   validates_presence_of :author
   validates_format_of :author, :with => /\A[\w\s\-]+\z/
-  validates_format_of :body, :with => /\A[\w\s\-\_!?\.]+\z/
+  validates_format_of :body, :with => /\A[\w\s\-\_!?\.\"\'\,]+\z/
   validates_length_of :body, :minimum => 3
 
   belongs_to :article
