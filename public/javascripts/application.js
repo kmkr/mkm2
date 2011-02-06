88517f8d26dcabcd229457d7e552039916eb47b5
Kris-Mikael Krister <krismikael@gmail.com>
2011-02-06 23:08:51 +0100

    hashchange for images

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,43 @@
 var galleryLoaded = false;
+var article_gallery;
+
+function findStartAtIndex() {
+    //var hash_image_name = window.location.split("=").pop();
+    var hash = window.location.hash;
+    if (hash.indexOf("image=") == -1) {
+      return 0;
+    }
+
+    var hash_image_name = hash.split("image=").pop();
+    var start_at_idx = 0;
+    var found_image = false;
+    $('.ad-thumb-list img').each(function(idx, image) {
+      var img_name = $(image).attr('src').split("/").pop().split("?").shift();
+
+      if (img_name == hash_image_name) {
+        start_at_idx = idx;
+        found_image = true;
+      }
+    });
+
+    if (!found_image) {
+      $('#no_such_hash').dialog({modal: true, width: 450, draggable: true});
+    }
+
+    return start_at_idx;
+}
+
 function loadGallery() {
   if (!galleryLoaded) {
-    jQuery('.ad-gallery').adGallery({
+
+    var startAtIndex = findStartAtIndex();
+
+    var galleries = jQuery('.ad-gallery').adGallery({
       loader_image: '/images/loader.gif',
       slideshow: {
         enable: false
       },
+      start_at_index: startAtIndex,
 
       callbacks: {
         init: function() {
@@ -13,9 +45,14 @@ function loadGallery() {
           this.preloadImage(1);
         },
         beforeImageVisible: function(new_image, old_image) {
-          // impl hashchange
         },
         afterImageVisible: function() {
+          var state = {},
+          id = 'image';
+          var image_name = this.images[this.current_index].image.split("/").pop().split("?").shift();
+          // Set the state!
+          state[ id ] = image_name;
+          $.bbq.pushState( state );
           var context = this;
           this.loading(true);
           this.preloadImage(this.current_index + 1,
@@ -27,6 +64,25 @@ function loadGallery() {
         }
       }
     });
+
+    article_gallery = galleries[0];
+
     galleryLoaded = true;
   }
 }
+
+$(function() {
+  // assumes a hashchange indicates a gallery view
+  // in case this is a fresh page load, fire the event to show correct image
+  $(window).bind( 'hashchange', function(e) {
+    /*
+    switchToTab(1);
+    loadGallery();
+    */
+    scrollToAndSwitchToTab(1);
+    var new_index = findStartAtIndex();
+    if (article_gallery.current_index != new_index) {
+      article_gallery.showImage(new_index);
+    }
+  });
+});
--- public/javascripts/article_switcher.js
+++ public/javascripts/article_switcher.js
@@ -5,8 +5,11 @@ jQuery(function() {
   $('#article_tabs').bind('tabsshow', function(evt, ui) { if (ui.index == 1) { loadGallery(); } } );
 
   scrollToAndSwitchToTab = function(idx) {
+    switchToTab(idx);
+    loadGallery();
+    $.scrollTo( '#article_tabs', 1400, {offset: -25, easing: 'easeOutElastic'} );
+  }
+  switchToTab = function(idx) {
       articleTabs.tabs('select', idx);
-      loadGallery();
-      $.scrollTo( '#article_tabs', 1400, {offset: -25, easing: 'easeOutElastic'} );
   }
 });
