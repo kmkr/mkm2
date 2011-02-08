d0dbd9fa94fa650186434883dd8286454d7552b8
Kris-Mikael Krister <krismikael@gmail.com>
2011-02-09 00:09:55 +0100

    improved css for navigation

--- public/javascripts/navigation.js
+++ public/javascripts/navigation.js
@@ -1,22 +1,34 @@
 jQuery(function() {
   jQuery('.continent').each(function(index, el) {
     var element = jQuery(el);
+    element.css({
+      "background": "#fff7ed",
+      "background": "-moz-linear-gradient(top,  #fff7ed,  #fff)",
+      "background": "-webkit-gradient(linear, left top, left bottom, from(#fff7ed), to(#fff))"});
     var ulElem = element.children('.countries').first();
     var animationOn = false;
     element.mouseenter(function() {
       if (!animationOn) {
         animationOn = true;
-        ulElem.slideDown(300, 'swing', function() {
+        ulElem.slideDown(250, 'swing', function() {
           animationOn = false;
         });
+        element.css("background", "");
+        element.animate({ 
+          "background-color": "#fff7ed",
+        }, 600, 'easeOutQuart');
       }
     });
 
     element.mouseleave(function() {
       animationOn = true;
-      ulElem.slideUp(300, 'swing', function() {
+      ulElem.slideUp(200, 'swing', function() {
         animationOn = false;
       });
+      element.css({
+      "background": "#fff7ed",
+      "background": "-moz-linear-gradient(top,  #fff7ed,  #fff)",
+      "background": "-webkit-gradient(linear, left top, left bottom, from(#fff7ed), to(#fff))"});
     });
   });
 
@@ -26,10 +38,16 @@ jQuery(function() {
     var ulElem = element.children('.articles').first();
     element.mouseenter(function() {
       ulElem.show();
+        element.css({ 
+          "background-color": "#fff7ed"
+        })
     });
 
     element.mouseleave(function() {
       ulElem.hide();
+        element.css({ 
+        "background-color": "#fff"
+        })
     });
   });
 });
