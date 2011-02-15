713e1c9f9e38de4650050bd110b69bc90318f872
Kris-Mikael Krister <krismikael@gmail.com>
2011-02-15 21:19:54 +0100

    must allow empty email

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ActiveRecord::Base
   validates_format_of :author, :with => /\A[\w\s\-]+\z/
   validates_format_of :body, :with => /\A[\w\s\-\_!?\.\"\'\,]+\z/
   validates_length_of :body, :minimum => 3
-  validates_format_of :email, :with => /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/
+  validates_format_of :email, :with => /(^$|^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$)/
 
   belongs_to :article
 end
