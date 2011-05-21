51173c02d7db5a3aff1fac1efb37bccfabd0fdf9
Kris-Mikael Krister <kris-mikael.krister@objectware.no>
2011-05-22 00:57:38 +0200

    zoom images when hover on image uploads

--- public/javascripts/new_article.js
+++ public/javascripts/new_article.js
@@ -16,3 +16,19 @@ jQuery(function() {
   });
   jQuery( "#sortable" ).disableSelection();
 });
+
+$(function() {
+	var origMaxHeight;
+   $('li.galleryitem img').mouseenter(function(e) {
+		  origMaxHeight = $(this).css('maxHeight');
+		  origHeight = $(this).css('height');
+		  origWidth = $(this).css('width');
+		  origLeft = $(this).css('left');
+		  origTop = $(this).css('top');
+		  $(this).css('zIndex', '300');
+        $(this).animate({ maxHeight: '400px' }, 100);
+    }).mouseleave(function(e) {
+        $(this).animate({ maxHeight: origMaxHeight }, 100);
+		  $(this).css('zIndex', '1');
+    });
+});
