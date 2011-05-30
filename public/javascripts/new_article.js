29fe84978a100c5b179be17dbbd5b01306dbc242
Kris-Mikael Krister <krismikael@gmail.com>
2011-05-30 23:46:48 +0200

    border back

--- public/javascripts/new_article.js
+++ public/javascripts/new_article.js
@@ -20,12 +20,13 @@ jQuery(function() {
 $(function() {
   var items = $('li.galleryitem img');
 	var origMaxHeight = items.first().css('maxHeight');
+  var origBorder = items.first().css("border");
    items.mouseenter(function(e) {
 		    $(this).css('zIndex', '300');
         $(this).animate({ maxHeight: '400px' }, 100);
     }).mouseleave(function(e) {
         $(this).animate({ maxHeight: origMaxHeight }, 100);
-        $(items).css({zIndex: '1', maxHeight: origMaxHeight});
+        $(items).css({zIndex: '1', maxHeight: origMaxHeight, border: origBorder});
     }).click(function(e) {
         $(this).css("border", "2px solid #ccc"); 
     });
