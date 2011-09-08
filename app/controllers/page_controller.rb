bab68b16ca97d14c9d3589d809b42bfb0691f581
Kris-Mikael Krister <kris-mikael.krister@objectware.no>
2011-09-08 13:12:29 +0200

    only use created at for fresh articles

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,7 +1,7 @@
 class PageController < ApplicationController
 
   def index
-    articles = Article.order('updated_at DESC').where('published_date IS NOT NULL').includes(:assets)
+    articles = Article.order('created_at DESC').where('published_date IS NOT NULL').includes(:assets)
     @newest_articles = articles.first.to_a
     @random_articles = (articles - @newest_articles).shuffle.first(4) 
     @title = "Welcome"
